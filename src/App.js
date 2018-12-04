5eddcc87e266cb9d8199db198c67bbd443fda3de
Iker Suen <apple@appledeMacBook-Air-2.local>
2018-12-05 00:11:35 +0800

    [iker] feat: add store in app.js for connection between component and reduce folder's state

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CounterGroup from './components/CounterGroup.js'
+import reducer from "./reducer";
+import {createStore} from "redux";
+import { Provider } from "react-redux";
+
+const store = createStore(reducer);
 
 class App extends Component {
   render() {
     return (
       <div>
+      <Provider store={store}> 
         <CounterGroup />
+      </Provider>
       </div>
     );
   }
